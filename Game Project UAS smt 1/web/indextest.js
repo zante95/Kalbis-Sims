5e5aac43575cdc6a2afd45da320c3d5b4510b22a
Johannes Dominic <johannes.dominic@gmail.com>
2015-01-06 15:53:18 +0700

    revisi 3 jojo 6/1/2014
    function promotionChecker belum bekerja

--- Game Project UAS smt 1/web/indextest.js
+++ Game Project UAS smt 1/web/indextest.js
@@ -11,6 +11,7 @@ var stamina_female = Math.floor((Math.random()*80)+20);
 var fun_female = Math.floor((Math.random()*80)+20);
 var social_female = Math.floor((Math.random()*80)+20);
 var mood_female=0.25*hunger_female + 0.25*stamina_female + 0.25*fun_female + 0.25*social_female;
+var progress = 0;
 
 function statusCheck(){
 	if(hunger_male <= 0) hunger_male = 0;
@@ -34,6 +35,7 @@ function statusCheck(){
 	if(fun_female >= 100) fun_female = 100;
 	if(social_female >= 100) social_female = 100;
 	if(mood_female >= 100) mood_female = 100;
+	if(progress >= 100) progress = 100;
 	
 	document.getElementById("hunger_male").value = hunger_male;
 	document.getElementById("stamina_male").value = stamina_male;
@@ -46,6 +48,7 @@ function statusCheck(){
 	document.getElementById("fun_female").value = fun_female;
 	document.getElementById("social_female").value = social_female;
 	document.getElementById("mood_female").value = mood_female;
+	document.getElementById("progress").value = progress;
 	var timeChecker1 = setTimeout(function(){statusCheck();},1000);
 }
 
@@ -60,17 +63,26 @@ function statusDecreaser(){
 	social_female -= 1;
 	mood_male = 0.25*hunger_male + 0.25*stamina_male + 0.25*fun_male + 0.25*social_male;
 	mood_female = 0.25*hunger_female + 0.25*stamina_female + 0.25*fun_female + 0.25*social_female;
-	var timeChecker2 = setTimeout(function(){statusDecreaser();},1000);
+	var timeChecker2 = setTimeout(function(){statusDecreaser();},3000);
+}
+
+function normalMale(){
+	var male_pict_normal = '<img src="image/nekoboy.png"/>';
+	document.getElementById("male_pict").innerHTML = male_pict_normal;
+}
+
+function normalFemale(){
+	var female_pict_normal = '<img src="image/nekogirl.png"/>';
+	document.getElementById("female_pict").innerHTML = female_pict_normal;
 }
 
 function gameStop(){
 	clearTimeout(timeChecker1);
 	clearTimeout(timeChecker2);
 	clearTimeout(timeChecker3);
-	var htmlSisip1 = '<img src="image/nekoboy.png"/>';
-	var htmlSisip2 = '<img src="image/nekogirl.png"/>';
-	document.getElementById("male_pict").innerHTML = htmlSisip1;
-	document.getElementById("female_pict").innerHTML = htmlSisip1;
+	clearTimeout(timeChecker4);
+	normalMale();
+	normalFemale();
 	/*loading();*/
 }
 
@@ -82,14 +94,98 @@ function gameOverChecker(){
 	}
 }
 
+function promotionChecker(){
+	if(progress == 100){
+	clearTimeout(timeChecker1);
+	clearTimeout(timeChecker2);
+	clearTimeout(timeChecker3);
+	clearTimeout(timeChecker4);
+	penanya = window.prompt("Masukkan penanya (laki/perempuan):");
+		if(penanya == "laki"){
+			pertanyaan = window.prompt("laki-laki bertanya. Masukkan pertanyaan untuk perempuan :");
+			jawaban = window.prompt ("Pertanyaan laki-laki adalah : "+ pertanyaan + ". Silahkan perempuan menjawab pertanyaan tersebut.");
+			cek = window.prompt ("Apakah jawaban tersebut benar? (Laki-laki memasukkan benar/salah.)");
+		}
+		if(penanya == "perempuan"){
+			pertanyaan = window.prompt("perempuan bertanya. Masukkan pertanyaan untuk laki-laki :");
+			jawaban = window.prompt ("Pertanyaan perempuan adalah : "+ pertanyaan + ". Silahkan laki-laki menjawab pertanyaan tersebut.");
+			cek = window.prompt ("Apakah jawaban tersebut benar? (Perempuan memasukkan benar/salah.)");
+		}
+	if(cek =="benar"){
+		progress = 0;
+		/*relationship status naik*/
+		}
+	if(cek == "salah"){
+		window.alert("jawaban salah, anda tidak naik level. Silahkan coba lagi pada kesempatan berikutnya.");
+		progress = Math.floor((Math.random()*75)+40);
+		/*relationship status tetap*/		
+		}
+	}
+	timeChecker4 = setTimeout(function(){promotionChecker();},1000);
+}
+
 function start(){
-statusCheck();
-statusDecreaser();
-gameOverChecker();
+	statusCheck();
+	statusDecreaser();
+	gameOverChecker();
+	promotionChecker();
+}
+
+function chat(){
+	social_male += 15;
+	social_female += 15;
+	progress += (mood_male / 10)+ (mood_female / 10); 
+	hunger_male -= 2;
+	stamina_male -= 2;
+	fun_male -= 2;
+	hunger_female -= 2;
+	stamina_female -= 2;
+	fun_female -= 2;
+}
+
+function eatMale(){
+	hunger_male += 15;
+	stamina_male -= 2;
+	fun_male -= 2;
+}
+function sleepMale(){
+	hunger_male -= 2;
+	stamina_male += 15;
+	fun_male -= 2;
+}
+
+function playMale(){
+	hunger_male -= 2;
+	stamina_male -= 2;
+	fun_male += 15;
+}
+
+function eatFemale(){
+	hunger_female += 15;
+	stamina_female -= 2;
+	fun_female -= 2;
+}
+
+function sleepFemale(){
+	hunger_female -= 2;
+	stamina_female += 15;
+	fun_female -= 2;
+}
+function playFemale(){
+	hunger_female -= 2;
+	stamina_female -= 2;
+	fun_female += 15;
 }
 
 function loading(){
-document.getElementById("button_start").addEventListener("click", start, false);
+	document.getElementById("button_start").addEventListener("click", start, false);
+	document.getElementById("eat_male").addEventListener("click", eatMale, false);
+	document.getElementById("eat_female").addEventListener("click", eatFemale, false); 
+	document.getElementById("sleep_male").addEventListener("click", sleepMale, false);
+	document.getElementById("sleep_female").addEventListener("click", sleepFemale, false);
+	document.getElementById("play_male").addEventListener("click", playMale, false);
+	document.getElementById("play_female").addEventListener("click", playFemale, false);
+	document.getElementById("button_chat").addEventListener("click", chat, false);
 }
 
 window.addEventListener("load", loading, false);
